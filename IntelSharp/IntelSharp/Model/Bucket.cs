d1241e543741cc09bad9daa2a4ba1aaa1d0e91e7
PaulusParssinen <paulus.parssinen01@edupori.fi>
2019-09-29 23:26:33 +0300

    Add constant bucket names

---
+++ IntelSharp/IntelSharp/Model/Bucket.cs
@@ -0,0 +1,19 @@
+﻿namespace IntelSharp.Model
+{
+    /// <summary>
+    /// Represents the constant bucket names. Availability depends on the user's license. 
+    /// </summary>
+    public static class Bucket
+    {
+        public const string PASTES = "pastes";
+        public const string DARKNET = "darknet";
+        public const string DARKNET_TOR = "darknet.tor";
+        public const string DARKNET_I2P = "darknet.i2p";
+        public const string WIKILEAKS = "leaks.public.wikileaks";
+        public const string PUBLIC_LEAKS = "leaks.public.general";
+        public const string DUMPSTER = "dumpster";
+        public const string WEB = "web.public";
+        public const string WEB_NORTHKOREA = "web.public.kp";
+        public const string WHOIS = "whois";
+    }
+}
