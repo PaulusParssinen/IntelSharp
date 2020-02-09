226d929bf07fa137df0dcd733af50c7da31957a4
PaulusParssinen <paulus.parssinen01@edupori.fi>
2020-02-09 02:37:56 +0200

    Finish File API implementation.

--- IntelSharp/IntelSharp/FileApi.cs
+++ IntelSharp/IntelSharp/FileApi.cs
@@ -6,6 +6,10 @@ using IntelSharp.Model;
 
 namespace IntelSharp
 {
+    /// TODO: Streams
+    /// <summary>
+    /// This API contains the methods to retrieve contents of an <see cref="Item"/>.
+    /// </summary>
     public class FileApi
     {
         private readonly IXApiContext _context;
@@ -15,34 +19,29 @@ namespace IntelSharp
             _context = context;
         }
 
-        public async Task<byte[]> PreviewAsync(Item item)
-        {
-            return await PreviewAsync(item.StorageId, DataType.Plaintext, MediaType.Text, item.Bucket).ConfigureAwait(false);
-        }
-        public async Task<byte[]> PreviewAsync(string storageId, DataType contentType, MediaType mediaType, string bucket)
-        {
-            var parameters = new Dictionary<string, object>
-            {
-                { "sid", storageId },
-                { "l", 20 },
-                { "f", 0 },
-                { "c", contentType }, //idk tbh, gon research more
-                { "m", mediaType },
-                { "b", bucket },
-            };
-
-            return await IXAPI.GetAsync<byte[]>(_context, "/file/preview", parameters).ConfigureAwait(false);
-        }
-
+        /// <summary>
+        /// Retrieves an <see cref="Item"/>'s raw data. 
+        /// <para/>
+        /// Maximum output sizes: <br/>
+        /// Free license: 10 MB <br/>
+        /// Professional license: 100 MB
+        /// </summary>
+        /// <param name="item">The item.</param>
+        /// <returns>The raw binary data of the item.</returns>
         public async Task<byte[]> ReadAsync(Item item)
         {
             return await ReadAsync(item.StorageId, item.Bucket).ConfigureAwait(false);
         }
-        public async Task<byte[]> ReadAsync(string storageId, string bucket)
+        /// <inheritdoc cref="ReadAsync(Item)"/>
+        /// <param name="storageId">The <see cref="Item.StorageId">storage identifier</see> of the item.</param>
+        /// <param name="bucket">The bucket in which the item is located.</param>
+        public async Task<byte[]> ReadAsync(string storageId, string bucket = default)
         {
+            const int OUTPUT_TYPE_RAW_BINARY = 0;
+
             var parameters = new Dictionary<string, object>
             {
-                { "type", 1 }, //TODO: ehhh
+                { "type", OUTPUT_TYPE_RAW_BINARY },
                 { "storageid", storageId },
                 { "bucket", bucket }
             };
@@ -50,21 +49,79 @@ namespace IntelSharp
             return await IXAPI.GetAsync<byte[]>(_context, "/file/read", parameters).ConfigureAwait(false);
         }
 
-        public async Task<byte[]> ViewAsync(Item item, string license) //TODO: why the license tho
+        /// <summary>
+        /// Fetches the data in wanted output format. Good use-case when displaying content to end-user.
+        /// <para/>
+        /// Maximum output sizes: <br/>
+        /// Free license: 4 MB <br/>
+        /// Professional license: 6 MB
+        /// </summary>
+        /// <param name="item">The item to be formatted.</param>
+        /// <param name="format">The output format data will be printed in.</param>
+        /// <param name="escapeHtml">Should the output HTML be escaped.</param>
+        /// <exception cref="InvalidOperationException"/>
+        /// <exception cref="Exception">The item was not found.</exception>
+        public async Task<byte[]> ViewAsync(Item item, ViewFormat format = ViewFormat.Text, bool escapeHtml = true)
         {
-            return await ViewAsync(item.StorageId, item.Bucket, license).ConfigureAwait(false);
+            return await ViewAsync(item.StorageId, item.Bucket, format, escapeHtml).ConfigureAwait(false);
         }
-        public async Task<byte[]> ViewAsync(string storageId, string bucket, string license)
+        /// <inheritdoc cref="ViewAsync(Item, ViewFormat, bool)"/>
+        /// <param name="storageId">The <see cref="Item.StorageId">storage identifier</see> of the item.</param>
+        /// <param name="bucket">The bucket in which the item is located.</param>
+        public async Task<byte[]> ViewAsync(
+            string storageId, 
+            string bucket, 
+            ViewFormat format = ViewFormat.Text,
+            bool escapeHtml = true)
         {
             var parameters = new Dictionary<string, object>
             {
-                { "f", 0 }, //TODO:
+                { "f", format },
                 { "storageid", storageId },
                 { "bucket", bucket },
-                { "license", license }
+                { "escape", escapeHtml ? 1 : 0 }
             };
 
             return await IXAPI.GetAsync<byte[]>(_context, "/file/view", parameters).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Provides a preview of item's contents.
+        /// </summary>
+        /// <param name="item">The item to be previewed.</param>
+        /// <param name="lineCount">The amount of lines to be shown in preview.</param>
+        /// <param name="escapeHtml">Should the output HTML be escaped.</param>
+        /// <exception cref="InvalidOperationException"/>
+        /// <returns>The preview of data.</returns>
+        public async Task<byte[]> PreviewAsync(Item item, int lineCount = 12, bool escapeHtml = true)
+        {
+            return await PreviewAsync(item.StorageId, item.Type, item.MediaType, item.Bucket, lineCount, escapeHtml).ConfigureAwait(false);
+        }
+        /// <inheritdoc cref="PreviewAsync(Item, int, bool)"/>
+        /// <param name="storageId">The <see cref="Item.StorageId">storage identifier</see> of the item.</param>
+        /// <param name="dataType">The low-level type representaton of the item's data type. Must be same as the source item's.</param>
+        /// <param name="mediaType">The high-level type representaton of the item's data type. Must be same as the source item's.</param>
+        /// <param name="bucket">The bucket in which the item is located.</param>
+        public async Task<byte[]> PreviewAsync(
+            string storageId,
+            DataType dataType,
+            MediaType mediaType,
+            string bucket,
+            int lineCount = 12, 
+            bool escapeHtml = true)
+        {
+            var parameters = new Dictionary<string, object>
+            {
+                { "sid", storageId },
+                { "b", bucket },
+                { "l", lineCount },
+                { "f", mediaType == MediaType.Picture ? 1 : 0 },
+                { "c", dataType },
+                { "m", mediaType },
+                { "e", escapeHtml ? 1 : 0 }
+            };
+
+            return await IXAPI.GetAsync<byte[]>(_context, "/file/preview", parameters).ConfigureAwait(false);
         }
     }
 }
---
+++ IntelSharp/IntelSharp/Model/Enum/ViewFormat.cs
@@ -0,0 +1,42 @@
+﻿namespace IntelSharp.Model
+{
+    /// <summary>
+    /// Represents the possible output formats for <see cref="FileApi.ViewAsync(Item, ViewFormat, bool)"/>
+    /// </summary>
+    public enum ViewFormat
+    {
+        /// <summary>
+        /// Text view, any non-printable characters shall be removed, UTF-8 encoding
+        /// </summary>
+        Text = 0,
+        /// <summary>
+        /// Hex view.
+        /// </summary>
+        Hex,
+        /// <summary>
+        /// Detects automatically whether to return hex or text view.
+        /// </summary>
+        AutoHexOrText,
+        /// <summary>
+        /// Picture view.
+        /// </summary>
+        Picture,
+
+        /// <summary>
+        /// HTML inline view. Content will be sanitized and modified!
+        /// </summary>
+        InlineHTML = 5,
+        /// <summary>
+        /// Text view of PDF. Content will be automatically converted
+        /// </summary>
+        PDFText,
+        /// <summary>
+        /// Text view of HTML.
+        /// </summary>
+        HTMLText,
+        /// <summary>
+        /// Text view of Word files (DOC/DOCX/RTF). 
+        /// </summary>
+        MSWordText,
+    }
+}
