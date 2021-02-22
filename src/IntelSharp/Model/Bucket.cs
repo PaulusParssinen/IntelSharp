a9c27ed8a26d14204d403a490229773eab27a719
PaulusParssinen <paulus.parssinen01@edupori.fi>
2021-02-22 22:05:46 +0200

    Add new buckets

--- src/IntelSharp/Model/Bucket.cs
+++ src/IntelSharp/Model/Bucket.cs
@@ -8,21 +8,23 @@
     public static class Bucket
     {
         /// <summary>
-        /// Pastes
+        /// [PRO] Pastes
         /// </summary>
         public const string PASTES = "pastes";
+
         /// <summary>
-        /// Darknet
+        /// [PRO] Darknet
         /// </summary>
         public const string DARKNET = "darknet";
         /// <summary>
-        /// Darknet » TOR
+        /// [PRO] Darknet » TOR
         /// </summary>
         public const string DARKNET_TOR = "darknet.tor";
         /// <summary>
-        /// Darknet » I2P
+        /// [PRO] Darknet » I2P
         /// </summary>
         public const string DARKNET_I2P = "darknet.i2p";
+
         /// <summary>
         /// Leaks » Public
         /// </summary>
@@ -30,23 +32,38 @@
         /// <summary>
         /// Leaks » Public » General
         /// </summary>
-        public const string LEAKS_GENERAL = "leaks.public.general";
+        public const string LEAKS_PUBLIC_GENERAL = "leaks.public.general";
         /// <summary>
         /// Leaks » Public » Wikileaks
         /// </summary>
-        public const string LEAKS_WIKILEAKS = "leaks.public.wikileaks";
+        public const string LEAKS_PUBLIC_WIKILEAKS = "leaks.public.wikileaks";
+        /// <summary>
+        /// [SECRET] Leaks » Private » General
+        /// </summary>
+        public const string LEAKS_PRIVATE_GENERAL = "leaks.private.general";
+        /// <summary>
+        /// [SECRET] Leaks » Bot Logs
+        /// </summary>
+        public const string LEAKS_LOGS = "leaks.logs";
+
         /// <summary>
         /// Dumpster
         /// </summary>
         public const string DUMPSTER = "dumpster";
+
         /// <summary>
-        /// WHOIS
+        /// [PRO] WHOIS
         /// </summary>
         public const string WHOIS = "whois";
+
         /// <summary>
         /// Web » Public
         /// </summary>
         public const string WEB_PUBLIC = "web.public";
+        /// <summary>
+        /// Web » Public » North Korea
+        /// </summary>
+        public const string WEB_PUBLIC_NORTH_KOREA = "web.public.kp";
         /// <summary>
         /// Web » Public » Russia
         /// </summary>
@@ -60,9 +77,69 @@
         /// </summary>
         public const string WEB_PUBLIC_COM = "web.public.com";
         /// <summary>
-        /// Web » Public » North Korea
+        /// Web » Public » .org
         /// </summary>
-        public const string WEB_PUBLIC_NORTHKOREA = "web.public.kp";
+        public const string WEB_PUBLIC_ORG = "web.public.org";
+        /// <summary>
+        /// Web » Public » .net
+        /// </summary>
+        public const string WEB_PUBLIC_NET = "web.public.net";
+        /// <summary>
+        /// Web » Public » .info
+        /// </summary>
+        public const string WEB_PUBLIC_INFO = "web.public.info";
+        /// <summary>
+        /// Web » Public » .eu
+        /// </summary>
+        public const string WEB_PUBLIC_EU = "web.public.eu";
+        /// <summary>
+        /// Web » Public » China
+        /// </summary>
+        public const string WEB_PUBLIC_CHINA = "web.public.cn";
+        /// <summary>
+        /// Web » Public » Northern Europe
+        /// </summary>
+        public const string WEB_PUBLIC_NORTHERN_EUROPE = "web.public.nord";
+        /// <summary>
+        /// Web » Public » Western Europe
+        /// </summary>
+        public const string WEB_PUBLIC_WESTERN_EUROPE = "web.public.we";
+        /// <summary>
+        /// Web » Public » Central and Eastern Europe
+        /// </summary>
+        public const string WEB_PUBLIC_CEE_EUROPE = "web.public.cee";
+        /// <summary>
+        /// Web » Public » Americas
+        /// </summary>
+        public const string WEB_PUBLIC_AMERICAS = "web.public.ams";
+        /// <summary>
+        /// Web » Public » Africa
+        /// </summary>
+        public const string WEB_PUBLIC_AFRICA = "web.public.af";
+        /// <summary>
+        /// Web » Public » Middle East and Asia
+        /// </summary>
+        public const string WEB_PUBLIC_MEA = "web.public.mea";
+        /// <summary>
+        /// Web » Public » Oceania
+        /// </summary>
+        public const string WEB_PUBLIC_OCEANIA = "web.public.oc";
+        /// <summary>
+        /// Web » Public » Technology
+        /// </summary>
+        public const string WEB_PUBLIC_TECHNOLOGY = "web.public.tech";
+        /// <summary>
+        /// Web » Public » Business
+        /// </summary>
+        public const string WEB_PUBLIC_BUSINESS = "web.public.business";
+        /// <summary>
+        /// Web » Public » Social
+        /// </summary>
+        public const string WEB_PUBLIC_SOCIAL = "web.public.social";
+        /// <summary>
+        /// Web » Public » Misc
+        /// </summary>
+        public const string WEB_PUBLIC_MISC = "web.public.misc";
         /// <summary>
         /// Web » Public » Decentralized TLDs
         /// </summary>
